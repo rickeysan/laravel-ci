6a51a2e5fff7c9ea8302450df05ed07bfc6816cb
rickeysan <rickeysan95@gmail.com>
2022-02-10 20:30:22 +0900

    Revert "わざと間違える"
    This reverts commit 0a5b27a377904b6e3ade9587cfd7f032fa31e111.

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -19,8 +19,7 @@ class ArticleControllerTest extends TestCase
     {
         $response = $this->get(route('articles.index'));
 
-        // $response->assertStatus(200)->assertViewIs('articles.index');
-        $response->assertStatus(400)->assertViewIs('articles.index');
+        $response->assertStatus(200)->assertViewIs('articles.index');
     }
     public function testGuestCreate(){
         $response = $this->get(route('articles.create'));
